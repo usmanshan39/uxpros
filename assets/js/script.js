4931df7a66b4d6632b667c26a2cc3986aa4f73db
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2022-01-11 21:19:34 +0500

    add half index page

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var menu_btn = document.querySelector("#menu-btn");
+var sidebar = document.querySelector("#sidebar");
+var container = document.querySelector(".my-container");
+menu_btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active-nav");
+  container.classList.toggle("active-cont");
+});
+
+var owl = $('.owl-carousel');
+         owl.owlCarousel({
+           margin: 10,
+           loop: true,
+           responsive: {
+             0: {
+               items: 1
+             },
+             600: {
+               items: 2
+             },
+             1000: {
+               items: 2
+             }
+           }
+         })
